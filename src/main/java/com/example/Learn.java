541b9ced313d1043e1efe8ef1398db64d51b9842
Utopia <gejunwei110@gmail.com>
2020-04-03 12:27:48 +0800

    add maven

---
+++ src/main/java/com/example/Learn.java
@@ -0,0 +1,12 @@
+package com.example;
+
+/**
+ * @author utopia on 2020/4/3
+ */
+public class Learn {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+
+}
